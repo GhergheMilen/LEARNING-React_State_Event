5fc02e763bac2723846d54be182bf323323ab5f9
GhMilen <gherghemilen@gmail.com>
2025-09-29 21:27:33 +0300

    Learned about the children keyword and reusability

--- src/App_copy.js
+++ src/App_copy.js
@@ -1,6 +1,10 @@
 import { useState } from "react";
 
-const messages = ["Learn React ⚛️", "Apply for jobs 💼", "Invest your new income 🤑"];
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
 
 export default function App() {
   return (
@@ -38,26 +42,22 @@ function Steps() {
           <p className="message">
             Step {step}: {messages[step - 1]}
           </p>
-
           <div className="buttons">
-            <Button textColor={"#fff"} bgColor={"#7950f2"} onClick={handlePrevious}>
-              <span>👈</span> Previous
-            </Button>
-
-            <Button textColor={"#fff"} bgColor={"#7950f2"} onClick={handleNext}>
-              Next <span>👉</span>
-            </Button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handlePrevious}
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handleNext}
+            >
+              Next
+            </button>
           </div>
         </div>
       )}
     </div>
-  );
-}
-
-function Button({ textColor, bgColor, onClick, children }) {
-  return (
-    <button style={{ backgroundColor: bgColor, color: textColor }} onClick={onClick}>
-      {children}
-    </button>
   );
 }
