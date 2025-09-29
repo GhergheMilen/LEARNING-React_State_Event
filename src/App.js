086eef829b4f4931be9289540df21c3d195cd475
GhMilen <gherghemilen@gmail.com>
2025-09-29 21:39:52 +0300

    Practice more with children keyword

--- src/App.js
+++ src/App.js
@@ -35,10 +35,7 @@ function Steps() {
             <div className={step >= 2 ? "active" : ""}>2</div>
             <div className={step >= 3 ? "active" : ""}>3</div>
           </div>
-          <p className="message">
-            Step {step}: {messages[step - 1]}
-          </p>
-
+          <StepMessage step={step}> {messages[step - 1]} </StepMessage>
           <div className="buttons">
             <Button textColor={"#fff"} bgColor={"#7950f2"} onClick={handlePrevious}>
               <span>👈</span> Previous
@@ -51,6 +48,15 @@ function Steps() {
         </div>
       )}
     </div>
+  );
+}
+
+function StepMessage({ step, children }) {
+  return (
+    <p className="message">
+      <h3>Step {step}</h3>
+      {children}
+    </p>
   );
 }
 
